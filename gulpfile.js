3f48c7c90fd5fb7dddbdbb637d41136f81a6a0bd
Kevin Li <kli@gilt.com>
2016-02-16 00:17:53 -0500

    Cleaning up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,9 @@ var browserify = require('browserify'); // Our app bundler
 var watchify = require('watchify'); // Update any source file and your bundle will be recompiled on the spot.
 var reactify = require('reactify'); // Transforms jsx to js
 var gulpif = require('gulp-if'); // conditionally run a task
-var uglify = require('gulp-uglify'); // minifier
-var streamify = require('gulp-streamify');
+var streamify = require('gulp-streamify'); // Wrap old Gulp plugins to support streams.
 var notify = require('gulp-notify');
 var concat = require('gulp-concat');
-var cssmin = require('gulp-cssmin');
 var gutil = require('gulp-util');
 var shell = require('gulp-shell');
 var glob = require('glob'); // Match files using the patterns
@@ -18,7 +16,7 @@ var connect = require('gulp-connect');
 
 // External dependencies you do not want to rebundle while developing,
 // but include in your application deployment
-var dependencies = [
+var external_dependencies = [
 	'react',
   'react-addons-test-utils'
 ];
@@ -30,23 +28,19 @@ var browserifyTask = function (options) {
 		entries: [options.src], // Only need initial file, browserify finds the rest
    	transform: [reactify], // We want to convert JSX to normal javascript
 		debug: options.env == 'development', // Gives us sourcemapping
-    fullPaths: options.env == 'development',
+    fullPaths: options.env == 'development', // Disables converting module ids into numerical indexes
 		cache: {}, packageCache: {}, // Requirement of watchify
 	});
 
-	// We set our dependencies as externals on our app bundler when developing
-	(options.env == 'development' ? dependencies : []).forEach(function (dep) {
-		appBundler.external(dep);
-	});
 
   // The rebundle process
-  var rebundle = function () {
+  var rebundleApp = function () {
     var start = Date.now();
     console.log('Building APP bundle');
+
     appBundler.bundle()
       .on('error', gutil.log)
       .pipe(source('main.js'))
-      .pipe(gulpif(options.env == 'production', streamify(uglify())))
       .pipe(gulp.dest(options.dest))
       .pipe(gulpif(options.env == 'development', livereload()))
       .pipe(notify(function () {
@@ -54,20 +48,16 @@ var browserifyTask = function (options) {
       }));
   };
 
-  // Fire up Watchify when developing
+
   if (options.env == 'development') {
+    // We set our dependencies as externals on our app bundler when developing
+    appBundler.external(external_dependencies);
+
+    // Fire up Watchify when developing
     appBundler.plugin(watchify);
-    appBundler.on('update', rebundle);
-  }
+    appBundler.on('update', rebundleApp);
 
-  rebundle();
-
-  // We create a separate bundle for our dependencies as they
-  // should not rebundle on file changes. This only happens when
-  // we develop. When deploying the dependencies will be included
-  // in the application bundle
-  if (options.env == 'development') {
-
+    // Setup test bundler
   	var testFiles = glob.sync('./specs/**/*-spec.js');
 		var testBundler = browserify({
 			entries: testFiles,
@@ -78,7 +68,7 @@ var browserifyTask = function (options) {
       plugin: [watchify]
 		});
 
-		testBundler.external(dependencies);
+		testBundler.external(external_dependencies);
 
   	var rebundleTests = function () {
   		var start = Date.now();
@@ -94,51 +84,55 @@ var browserifyTask = function (options) {
   	};
 
     testBundler.on('update', rebundleTests);
-    rebundleTests();
 
+    // Setup vendor bundler
     var vendorsBundler = browserify({
       debug: true,
-      require: dependencies
+      require: external_dependencies // Make file(s) available from outside the bundle
     });
 
-    // Run the vendor bundle
-    var start = new Date();
-    console.log('Building VENDORS bundle');
-    vendorsBundler.bundle()
-      .on('error', gutil.log)
-      .pipe(source('vendors.js'))
-      .pipe(gulpif(options.env == 'production', streamify(uglify())))
-      .pipe(gulp.dest(options.dest))
-      .pipe(notify(function () {
-        console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
-      }));
+    var bundleVendors = function () {
+      var start = new Date();
+        console.log('Building VENDORS bundle');
+        vendorsBundler.bundle()
+          .on('error', gutil.log)
+          .pipe(source('vendors.js'))
+          .pipe(gulp.dest(options.dest))
+          .pipe(notify(function () {
+            console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
+          }));
+    };
+
 
   }
 
-}
+  rebundleApp();
+  rebundleTests();
+  bundleVendors();
+
+};
 
 var cssTask = function (options) {
-    if (options.env == 'development') {
-      var run = function () {
-        console.log(arguments);
-        var start = new Date();
-        console.log('Building CSS bundle');
-        gulp.src(options.src)
-          .pipe(concat('main.css'))
-          .pipe(gulp.dest(options.dest))
-          .pipe(notify(function () {
-            console.log('CSS bundle built in ' + (Date.now() - start) + 'ms');
-          }));
-      };
-      run();
-      gulp.watch(options.src, run);
-    } else {
+  if (options.env == 'development') {
+    var rebundleCss = function () {
+      var start = new Date();
+      console.log('Building CSS bundle');
       gulp.src(options.src)
         .pipe(concat('main.css'))
-        .pipe(cssmin())
-        .pipe(gulp.dest(options.dest));
-    }
-}
+        .pipe(gulp.dest(options.dest))
+        .pipe(notify(function () {
+          console.log('CSS bundle built in ' + (Date.now() - start) + 'ms');
+        }));
+    };
+
+    gulp.watch(options.src, rebundleCss);
+    rebundleCss();
+  } else {
+    gulp.src(options.src)
+      .pipe(concat('main.css'))
+      .pipe(gulp.dest(options.dest));
+  }
+};
 
 // Starts our development workflow
 gulp.task('development', function () {
